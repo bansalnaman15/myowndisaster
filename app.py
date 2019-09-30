7562d2f997ffee8b5785c25a87f136d524481f11
unknown <Baman>
2019-10-01 01:37:02 +0530

    learnign git

--- app.py
+++ app.py
@@ -9,42 +9,39 @@ from bson import ObjectId
 from bson.json_util import dumps
 
 
-# class JSONEncoder(json.JSONEncoder):
-#     def default(self, o):
-#         if isinstance(o, ObjectId):
-#             return str(o)
-#         if isinstance(o, datetime):
-#             return str(o)
-#         return json.JSONEncoder.default(self, o)
+class JSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        if isinstance(o, datetime):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
+
 
 def sensor():
     """ Function for test purposes. """
     print("Scheduler is alive!")
-    user=userCol.find_one({"name":"Naman"})
-    print(user)
-    if "online" in user.keys():
-        if (user["online"]==True):
-            user["online"]=False
-        else: user["online"]=True
-    else: user["online"]=True
-    userCol.update_one({"name":"Naman"},{"$set":user})
+
+
+# work has to be done here
+
 scheduler = BackgroundScheduler(daemon=True)
 scheduler.add_job(sensor, 'interval', seconds=30)
 scheduler.start()
 
 app = Flask(__name__)
-# app.json_encoder = JSONEncoder
+app.json_encoder = JSONEncoder
 auth = HTTPBasicAuth()
-users = {
-    "john": generate_password_hash("hello"),
-    "susan": generate_password_hash("bye"),
-    "naman":generate_password_hash("bansal")
-}
-
-
 app.config["MONGO_URI"] = "mongodb+srv://bansalnaman:bansal@self-lkibt.mongodb.net/OwnDisaster?retryWrites=true&w=majority"
 mongo = PyMongo(app)
 userCol = mongo.db.users
+
+users = {
+    "john": generate_password_hash("hello"),
+    "susan": generate_password_hash("bye"),
+    "naman": generate_password_hash("bansal")
+}
+
 
 @auth.verify_password
 def verify_password(username, password):
@@ -52,15 +49,17 @@ def verify_password(username, password):
         return check_password_hash(users.get(username), password)
     return False
 
+
 @app.route('/')
 @auth.login_required
 def hello_world():
-    return ('Hello, World!'),200
+    return ('Hello, World!'), 200
+
 
 @app.route('/dedo')
 def userDedo():
     users = list(userCol.find({}))
-    return jsonify({"data":users}),200
+    return jsonify({"data": users}), 200
 
 
 if __name__ == '__main__':
