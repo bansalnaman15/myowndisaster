574d1848331a9e12ad3c223cca72f6b2caa5e7db
NAMAN <bansalnaman15@gmail.com>
2019-10-08 23:12:20 +0530

    nibbbbbb

--- mail.py
+++ mail.py
@@ -6,15 +6,15 @@ mail=Mail(app)
 
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = "nanchalchadwani@gmail.com"
-app.config['MAIL_PASSWORD'] = "Mayank123"
+app.config['MAIL_USERNAME'] = "namanash2015@gmail.com"
+app.config['MAIL_PASSWORD'] = "Naman2015"
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
 
 @app.route("/")
 def index():
-   msg = Message('Hello', sender = 'nanchalchadwani@gmail.com', recipients = ['bansalnaman15@gmail.com'])
+   msg = Message('Hello', sender = 'namanash2015@gmail.com', recipients = ['bansalnaman15@gmail.com'])
    msg.body = "Hello Naman"
    mail.send(msg)
    return "Sent"
