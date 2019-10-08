4b97225f6a2597436d17e9a36126ba61e8235194
NAMAN <bansalnaman15@gmail.com>
2019-10-08 22:48:08 +0530

    mail.py

--- mail.py
+++ mail.py
@@ -6,16 +6,16 @@ mail=Mail(app)
 
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = "yourmail"
-app.config['MAIL_PASSWORD'] = "yourpass"
+app.config['MAIL_USERNAME'] = "nanchalchadwani@gmail.com"
+app.config['MAIL_PASSWORD'] = "Mayank123"
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
 
 @app.route("/")
 def index():
-   msg = Message('Hello', sender = 'sender', recipients = ['recipient'])
-   msg.body = "Hello Flask message sent from Flask-Mail"
+   msg = Message('Hello', sender = 'nanchalchadwani@gmail.com', recipients = ['bansalnaman15@gmail.com'])
+   msg.body = "Hello Naman"
    mail.send(msg)
    return "Sent"
 
