4513194f51ecc0ac3a46c983e10709cde9be7adc
NAMAN <bansalnaman15@gmail.com>
2019-10-08 23:29:50 +0530

    deploylast:

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask import Flask
 
 app =Flask(__name__)
 
-app.route("/")
+app.route('/')
 def home():
-   return "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+   return (aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa),200
 
 
 if __name__ == '__main__':
